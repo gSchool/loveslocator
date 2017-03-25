fe617b5a90a982a2e1ab8e98dc00d0584c835e5b
Brent Gardner <bgardner@squarelabs.net>
2017-03-25 15:25:28 -0600

    Formatted search results

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -11,8 +11,30 @@ export default class ResultList extends Component {
     get results() {
         return this.props.locations && this.props.locations.map((location, i) => {
             return (
-                <div key={i}>
-                    Store #{location.SiteId}
+                <div key={i} className="resultBlock">
+                    <div style={{ display: "flex", flexDirection: "column" }}>
+                        <div style={{ display: "flex", flexDirection: "row" }}>
+                            <div>
+                                <img src="img/logo_icon.png"/>
+                            </div>
+                            <div style={{ display: "flex", flexDirection: "column", flex: "2 0 0", alignItems: "flex-start" }}>
+                                <h4>
+                                    <span>Exit {location.ExitNumber}</span>
+                                    <span>{location.City}, {location.State}</span>
+                                    <span>{location.Highway}</span>
+                                </h4>
+                                <h5>
+                                    <span>{location.Address1}, </span>
+                                    <span>{location.City}, </span>
+                                    <span>{location.State}</span>
+                                    <span>{location.Zip}</span>
+                                </h5>
+                            </div>
+                            <div style={{flex: "1 0 0"}}>
+                                Store #{location.SiteId}
+                            </div>
+                        </div>
+                    </div>
                 </div>
             )
         });
